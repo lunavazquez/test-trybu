69fd0ef02891413986ef4f4bbb5ecaf20f698471
Mextli Vazquez <luna@MacBook-Pro-de-Mextli.local>
2020-06-15 22:44:49 -0500

    chore: instalacion de dependencias y configuracion de eslint y prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  jsxBracketSameLine: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+  singleQuote: true,
+  printWidth: 80,
+  useTabs: false,
+  semi: true,
+};
