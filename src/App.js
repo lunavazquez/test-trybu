c9396ddfcdb440c1f421f42452cec44a9d541bf5
Mextli Vazquez <luna.vazquezv@gmail.com>
2020-06-16 23:10:13 -0500

    se agrega funcionalidad de filtrado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import Header from './components/Header';
 import Card from './components/Card';
 import './App.css';
 
@@ -18,28 +19,48 @@ const EXCHANGE_POKEMONS = gql`
 `;
 
 export default function App() {
+  const [search, setSearch] = useState('');
   const { loading, error, data } = useQuery(EXCHANGE_POKEMONS);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
   // console.log(data.pokemons);
   // destructuring
   const { pokemons } = data;
+
+  function handelNameSearch(e) {
+    setSearch(e.target.value);
+  }
+  // console.log(search);
   return (
     <section className="container">
+      <Header value={search} onChange={handelNameSearch} />
       <article className="pokemon_container">
-        {pokemons.map((pokemon) => {
-          // destructuring
-          const { id, image, types, name, number } = pokemon;
-          return (
-            <Card
-              id={number}
-              key={id}
-              img={image}
-              name={name}
-              type={types[0].toLowerCase()}
-            />
-          );
-        })}
+        {pokemons
+          .filter((pokemon) => {
+            // console.log(search.length);
+            // se ejecuta cuando search tiene un valor
+            if (search) {
+              return pokemon.name
+                .toLowerCase()
+                .includes(search.toLocaleLowerCase());
+            }
+            // regresa arreglo completo
+            return true;
+          })
+          .map((pokemon) => {
+            // console.log(pokemon);
+            // destructuring
+            const { id, image, types, name, number } = pokemon;
+            return (
+              <Card
+                id={number}
+                key={id}
+                img={image}
+                name={name}
+                type={types[0].toLowerCase()}
+              />
+            );
+          })}
       </article>
     </section>
   );
