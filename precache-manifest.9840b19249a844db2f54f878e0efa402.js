5f5d89692c11db488cd29dc189f00ae591d1e906
Mextli Vazquez <luna.vazquezv@gmail.com>
2020-06-16 23:26:40 -0500

    Updates

---
+++ precache-manifest.9840b19249a844db2f54f878e0efa402.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49fd8fd3a6d38c51e87051739d8deb7",
+    "url": "/test-trybu/index.html"
+  },
+  {
+    "revision": "6dea45850dbd0f0b5e64",
+    "url": "/test-trybu/static/css/main.16a6f8cd.chunk.css"
+  },
+  {
+    "revision": "658284724232d9579445",
+    "url": "/test-trybu/static/js/2.6c2f1c41.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/test-trybu/static/js/2.6c2f1c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dea45850dbd0f0b5e64",
+    "url": "/test-trybu/static/js/main.cb555e12.chunk.js"
+  },
+  {
+    "revision": "b5e15b2a940bf04f0260",
+    "url": "/test-trybu/static/js/runtime-main.196c2737.js"
+  },
+  {
+    "revision": "203d635c62e6a0c8435d06bf507c8f72",
+    "url": "/test-trybu/static/media/pokemon.203d635c.svg"
+  },
+  {
+    "revision": "a88f77554bd97c8632e77132d37d6e8e",
+    "url": "/test-trybu/static/media/search.a88f7755.svg"
+  }
+]);
